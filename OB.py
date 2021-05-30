8784322ec3d057f0527b8acba483cc06ae23ddad
HTXone <799319150@qq.com>
2021-05-30 17:31:54 +0800

    val1.0

---
+++ OB.py
@@ -0,0 +1,17 @@
+a = '/"#{.+((7|)y|7.(|)7"(,~7--++-{{*~//'
+print(ord('7')^ord('-'))
+print(ord('f'),ord(')'))
+x = ""
+for i in a:
+    x = x+((chr)(ord(i)^26))
+print(x)
+
+
+# List2 = []
+# for i in range(10):
+#     List2.append(i)
+#
+# for i in range(6):
+#     List2.append(chr(ord('a')+i))
+
+# print(List2)
